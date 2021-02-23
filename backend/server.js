6ccdf4d3db6ae3dea74fec54b9af910d3cfd1985
Bennyyoung <benjamineffiong124@yahoo.com>
2021-02-24 00:38:13 +0100

    My third commit

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,13 @@ app.use('/auth', userRouter)
 //     app.use
 // }
 
+if (process.env.NODE_ENV === "production") {
+ app.use(express.static("build"));
+ app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+ });
+}
+
 app.listen(PORT, () => {
  console.log(`Server is running on port: ${PORT}`);
 });
