d1eb393a30873c6761015664c382d0c81f60f8e4
Bennyyoung <benjamineffiong124@yahoo.com>
2021-04-13 02:53:25 +0100

    Commented process.env.NODE_ENV in the server.js

--- backend/server.js
+++ backend/server.js
@@ -65,12 +65,12 @@ app.use('/auth', userRouter)
 //     app.use
 // }
 
-if (process.env.NODE_ENV === "production") {
- app.use(express.static("client/build"));
- app.get("*", (req, res) => {
-   res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
- });
-}
+// if (process.env.NODE_ENV === "production") {
+//  app.use(express.static("client/build"));
+//  app.get("*", (req, res) => {
+//    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+//  });
+// }
 
 app.listen(PORT, () => {
  console.log(`Server is running on port: ${PORT}`);
