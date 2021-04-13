a2a88a2dd6a0cd32a6d3f95621cde2da3c10e863
Bennyyoung <benjamineffiong124@yahoo.com>
2021-04-13 17:43:34 +0100

    Added the config folder

---
+++ backend/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+ mongoURI:'mongodb+srv://sunrise-keepers-school-management-system:sunrisekeepers@sunrise-keepers-cluster.zulpb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
